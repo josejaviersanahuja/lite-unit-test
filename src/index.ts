8db3c45d39101e8c83394079c9c1b085956ebf3c
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-03-07 00:31:59 +0000

    add eslint prettier

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,87 @@
-import cli from './cli'
-import assert from 'node:assert'
+import cli from "./cli";
+import assert from "node:assert";
 
 export type LightTestModule = {
-  [key: string]: (cb: Function) => void
-}
+  [key: string]: (cb: () => void) => void;
+};
 
 export type LightTest = {
-  [key: string]: LightTestModule
-}
+  [key: string]: LightTestModule;
+};
 
 class TestEngine {
-  tests: LightTest
-  constructor () {
-    this.tests = {}
+  tests: LightTest;
+  constructor() {
+    this.tests = {};
   }
 
-  addTestModule (name: string, module: LightTestModule) {
-    this.tests[name] = module
+  addTestModule(name: string, module: LightTestModule) {
+    this.tests[name] = module;
   }
 
-  countTests () {
-    let counter = 0
+  countTests() {
+    let counter = 0;
     for (const key in this.tests) {
       if (Object.hasOwnProperty.call(this.tests, key)) {
-        const subTests = this.tests[key]
+        const subTests = this.tests[key];
         for (const testName in subTests) {
           if (Object.hasOwnProperty.call(subTests, testName)) {
-            counter++
+            counter++;
           }
         }
       }
     }
-    return counter
+    return counter;
   }
 
-  async runTests () {
-    const errors: { name: string, error: string }[] = []
-    let successes = 0
-    const limit = this.countTests()
-    let counter = 0
+  async runTests() {
+    const errors: { name: string; error: string }[] = [];
+    let successes = 0;
+    const limit = this.countTests();
+    let counter = 0;
 
     for (const key in this.tests) {
       if (Object.hasOwnProperty.call(this.tests, key)) {
-        const subTest = this.tests[key]
+        const subTest = this.tests[key];
         for (const testName in subTest) {
           if (Object.hasOwnProperty.call(subTest, testName)) {
             (function () {
               // const tmpTestName = testName
-              const testValue = subTest[testName]
+              const testValue = subTest[testName];
               // Call the test
               try {
-                testValue(function () {
+                testValue(() => {
                   // if it calls back without throwing, then it succeded. log in green
-                  console.log('\x1b[32m%s\x1b[0m', testName)
+                  console.log("\x1b[32m%s\x1b[0m", testName);
                   // console.log('\x1b[33m%s\x1b[0m', key + ': ','\x1b[32m%s\x1b[0m', testName)
-                  counter++
-                  successes++
+                  counter++;
+                  successes++;
                   if (counter === limit) {
-                    cli.produceTestReport(limit, successes, errors)
+                    cli.produceTestReport(limit, successes, errors);
                   }
-                })
+                });
               } catch (error) {
                 // if it throws it failed
                 if (error instanceof assert.AssertionError) {
                   errors.push({
                     name: testName,
-                    error: error.toString()
-                  })
-                  console.log(
-                    '\x1b[31m%s\x1b[0m', testName)
-                  counter++
+                    error: error.toString(),
+                  });
+                  console.log("\x1b[31m%s\x1b[0m", testName);
+                  counter++;
                   if (counter === limit) {
-                    cli.produceTestReport(limit, successes, errors)
+                    cli.produceTestReport(limit, successes, errors);
                   }
                 } else {
-                  assert(typeof error === 'object' && error !== null, 'Error should be an object')
-                  console.log('QUE PASA?', error.toString())
-                  throw error
+                  assert(
+                    typeof error === "object" && error !== null,
+                    "Error should be an object"
+                  );
+                  console.log("QUE PASA?", error.toString());
+                  throw error;
                 }
               }
-            })()
+            })();
           }
         }
       }
@@ -87,4 +89,4 @@ class TestEngine {
   }
 }
 
-export default new TestEngine()
+export default new TestEngine();
